231f653347dc316c37e8e06e140c6f1b7c4fb5b8
dereksmith83 <mdsmith1983@gmail.com>
2018-11-21 21:14:14 -0600

    updated seeds file with events & seeded database

--- !Progress.rb
+++ !Progress.rb
@@ -402,21 +402,30 @@ Events
     # git push origin master
 
   # 2 - create Event model 
-    rails generate model Event name:string description:text user_id:integer venue_id:integer start_date:datetime end_date:datetime status:string sales:integer 
+    # rails generate model Event name:string description:text user_id:integer venue_id:integer start_date:datetime end_date:datetime status:string sales:integer 
 
     #double check migration
-    rails db:migrate
+    # rails db:migrate
 
     #git save
-    git add --all
-    git commit -m 'created Event model'
-    git push origin master
+    # git add --all
+    # git commit -m 'created Event model'
+    # git push origin master
 
-  3 - update seeds file with events & seed database
-  event = Event.create(name: '', description: '', user_id: n, venue_id: n, start_date: n, end_date: n, status: '', sales: n)
+  # 3 - update seeds file with events & seed database
+    # event = Event.create(name:  "Effie Gray", description:  "Film Screening", user_id:  4,  venue_id: 4,  start_date: "2018-01-20 19:00:00.000",  end_date: "2018-01-20 23:59:59.000",  status: "sale_ended", sales:  33)
+    # event = Event.create(name:  "Mermaids", description:  "Live Play",  user_id:  4,  venue_id: 1,  start_date: "2018-03-09 19:00:00.000",  end_date: "2018-03-09 23:59:59.000",  status: "sale_ended", sales:  53)
+    # event = Event.create(name:  "Charlie Chan's Courage", description:  "Sing a long",  user_id:  2,  venue_id: 9,  start_date: "2018-07-19 19:00:00.000",  end_date: "2018-07-19 23:59:59.000",  status: "sale_ended", sales:  96)
+    # event = Event.create(name:  "Fantasia", description:  "viewing",  user_id:  2,  venue_id: 10, start_date: "2018-11-30 19:00:00.000",  end_date: "2018-11-30 23:59:59.000",  status: "on_sale",  sales:  81)
+    # event = Event.create(name:  "Trick or Treat", description:  "live event", user_id:  3,  venue_id: 2,  start_date: "2018-12-02 19:00:00.000",  end_date: "2018-12-02 23:59:59.000",  status: "on_sale",  sales:  85)
+    # event = Event.create(name:  "Scandal Sheet",  description:  "Stage adaptation", user_id:  1,  venue_id: 7,  start_date: "2019-01-18 19:00:00.000",  end_date: "2018-01-18 23:59:59.000",  status: "on_sale",  sales:  33)
+    # event = Event.create(name:  "Detention",  description:  "improv revue", user_id:  1,  venue_id: 4,  start_date: "2019-03-11 19:00:00.000",  end_date: "2019-03-11 23:59:59.000",  status: "pending",  sales:  31)
+    # event = Event.create(name:  "Rock 'n' Roll Nightmare",  description:  "concert",  user_id:  2,  venue_id: 10, start_date: "2019-05-06 19:00:00.000",  end_date: "2019-05-06 23:59:59.000",  status: "pending",  sales:  70)
+    # event = Event.create(name:  "Wheels on Meals",  description:  "charity",  user_id:  3,  venue_id: 8,  start_date: "2019-07-17 19:00:00.000",  end_date: "2019-07-17 23:59:59.000",  status: "pending",  sales:  33)
+    # event = Event.create(name:  "The Adventures of Picasso",  description:  "art display",  user_id:  3,  venue_id: 3,  start_date: "2019-09-08 19:00:00.000",  end_date: "2019-09-08 23:59:59.000",  status: "pending",  sales:  55)
 
-  #stop server
-  rails db:reset
+    #stop server
+    # rails db:reset
 
   #git save
   git add --all
