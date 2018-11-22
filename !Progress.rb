656181cef2c3677dffe8cad7de65c5556db5040a
dereksmith83 <mdsmith1983@gmail.com>
2018-11-21 19:06:25 -0600

    updated seeds file with venues & seeded database

--- !Progress.rb
+++ !Progress.rb
@@ -271,45 +271,63 @@ Venue
     # git commit -m 'created venues_controller'
     # git push origin master
 
-2 - create Venue model 
-rails generate model Venue name:string address:string city:string state:string zip:string website:string contact:string email:string phone:string capacity:integer      
+  #2 - create Venue model 
+    # rails generate model Venue name:string address:string city:string state:string zip:string website:string contact:string email:string phone:string capacity:integer      
 
-#git save
-git add --all
-git commit -m 'created Venue model'
-git push origin master
+    # #git save
+    # git add --all
+    # git commit -m 'created Venue model'
+    # git push origin master
 
-3 - update seeds file with venues & seed database
-venue = Venue.create(name: '', address: '', city: '', state: '', zip: '', website: '', contact: '', email: '', phone: '', capacity: n)
+  #3 - update seeds file with venues & seed database
+    # venue = Venue.create(name:  "Aldie's",  address:  "91 Arrowood Court",  city: "Largo",  state:  "FL", zip:  "34643",  website:  "www.Aldies.com", contact:  "Chadd",  email:  "Chadd@Aldies.com", phone:  "727-729-4386", capacity: 227)
 
-#stop server
-rails db:reset
+    # venue = Venue.create(name:  "Zoppo's",  address:  "76396 Weeping Birch Street", city: "Carlsbad", state:  "CA", zip:  "92013",  website:  "www.Zoppos.com", contact:  "Melisenda",  email:  "Melisenda@Zoppos.com", phone:  "760-715-2618", capacity: 221)
 
-#git save
-git add --all
-git commit -m 'updated seeds file with venues & seeded database'
-git push origin master
+    # venue = Venue.create(name:  "Gellett's",  address:  "14 Marcy Junction",  city: "Fresno", state:  "CA", zip:  "93709",  website:  "www.Gelletts.com", contact:  "Daile",  email:  "Daile@Gelletts.com", phone:  "559-430-6277", capacity: 104)
 
-4 - add venues_controller methods & association
+    # venue = Venue.create(name:  "Prinne's", address:  "7066 Loomis Place",  city: "Virginia Beach", state:  "VA", zip:  "23471",  website:  "www.Prinnes.com",  contact:  "Emanuele", email:  "Emanuele@Prinnes.com", phone:  "757-592-3848", capacity: 112)
+
+    # venue = Venue.create(name:  "Pexton's", address:  "8522 Farmco Junction", city: "Los Angeles",  state:  "CA", zip:  "90081",  website:  "www.Pextons.com",  contact:  "Harman", email:  "Harman@Pextons.com", phone:  "213-763-3465", capacity: 82)
+
+    # venue = Venue.create(name:  "Duro's", address:  "4720 Green Lane",  city: "Tacoma", state:  "WA", zip:  "98417",  website:  "www.Duros.com",  contact:  "Alvin",  email:  "Alvin@Duros.com",  phone:  "253-413-2718", capacity: 280)
+
+    # venue = Venue.create(name:  "Rowley's", address:  "96 Bayside Terrace", city: "Boise",  state:  "ID", zip:  "83722",  website:  "www.Rowleys.com",  contact:  "Marie-ann",  email:  "Marie-ann@Rowleys.com",  phone:  "208-458-2302", capacity: 111)
+
+    # venue = Venue.create(name:  "Volk's", address:  "4764 Swallow Crossing",  city: "Naples", state:  "FL", zip:  "33963",  website:  "www.Volks.com",  contact:  "Theodosia",  email:  "Theodosia@Volks.com",  phone:  "239-894-8801", capacity: 228)
+
+    # venue = Venue.create(name:  "Redd's", address:  "755 South Parkway",  city: "New Orleans",  state:  "LA", zip:  "70165",  website:  "www.Redds.com",  contact:  "Vivie",  email:  "Vivie@Redds.com",  phone:  "504-637-0038", capacity: 197)
+
+    # venue = Venue.create(name:  "Burbage's",  address:  "1384 Ridge Oak Way", city: "Las Vegas",  state:  "NV", zip:  "89166",  website:  "www.Burbages.com", contact:  "Julio",  email:  "Julio@Burbages.com", phone:  "702-169-2703", capacity: 93)
+    
+    #stop server
+    # rails db:reset
+
+    #git save
+      # git add --all
+      # git commit -m 'updated seeds file with venues & seeded database'
+      # git push origin master
+
+  # 4 - add venues_controller methods & association
 
 
-#git save
-git add --all
-git commit -m 'added venues_controller methods & association'
-git push origin master
+    #git save
+    git add --all
+    git commit -m 'added venues_controller methods & association'
+    git push origin master
 
 
-5 - add venues restful routes & test
-get"/venues" => "venues#index"
-post"/venues" => "venues#create"
-get"/venues/:id" => "venues#show"
-patch"/venues/:id" => "venues#update"
-delete"/venues/:id" => "venues#destroy"
+  #5 - add venues restful routes & test
+    get"/venues" => "venues#index"
+    post"/venues" => "venues#create"
+    get"/venues/:id" => "venues#show"
+    patch"/venues/:id" => "venues#update"
+    delete"/venues/:id" => "venues#destroy"
 
-#git save
-git add --all
-git commit -m 'added venues restful routes & tested successfully'
-git push origin master
+    #git save
+    git add --all
+    git commit -m 'added venues restful routes & tested successfully'
+    git push origin master
 
 
 Orders
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_130440) do
+ActiveRecord::Schema.define(version: 2018_11_19_222035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,21 @@ ActiveRecord::Schema.define(version: 2018_11_18_130440) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "website"
+    t.string "contact"
+    t.string "email"
+    t.string "phone"
+    t.integer "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
